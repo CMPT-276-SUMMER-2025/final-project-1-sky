9723e1747c9554f06956cbe7958623cef1214d3e
Daniel <daniel101shi@gmail.com>
2025-07-17 10:55:07 -0700

    manual installation of HeroUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "../components/ui/footer";
+import { Providers } from "./heroUI/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +35,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          {children}
+        </Providers>
         <Footer /> 
       </body>
     </html>
