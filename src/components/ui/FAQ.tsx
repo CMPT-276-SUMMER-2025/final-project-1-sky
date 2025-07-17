51993e3e5f0c967aac43ab1284fd8ed04815a807
Daniel <daniel101shi@gmail.com>
2025-07-17 11:01:53 -0700

    updated FAQ to remove default text

--- src/components/ui/FAQ.tsx
+++ src/components/ui/FAQ.tsx
@@ -1,9 +1,6 @@
 import {Accordion, AccordionItem} from "@heroui/react";
 
 export default function FAQ() {
-  const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-
   return (
     <Accordion>
       <AccordionItem key="1" aria-label="Question A" title="Question A">
