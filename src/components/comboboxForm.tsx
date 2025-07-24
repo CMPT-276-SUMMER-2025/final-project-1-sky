f9d2399605283c4b0d5b9b82d2293255f4a89326
laydab <aydenbadyal@gmail.com>
2025-07-24 12:04:47 -0700

    remove unused toast import from comboboxForm component

--- src/components/comboboxForm.tsx
+++ src/components/comboboxForm.tsx
@@ -3,7 +3,6 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Check, ChevronsUpDown } from "lucide-react"
 import { useForm } from "react-hook-form"
-import { toast } from "sonner"
 import { z } from "zod"
 import { useRouter } from "next/navigation"
 
